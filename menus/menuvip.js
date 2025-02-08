cd3afb90d0bc498cdbc45f266ef092f480ab0447
Powerapis7 <powerapis7@gmail.com>
2025-02-07 22:26:18 -0300

    Add files via upload

---
+++ menus/menuvip.js
@@ -0,0 +1,144 @@
+exports.menuvip = (comando, prefix, hora, data, pushname, totalcmd) => {
+return `
+╭───────────────╮
+│      👑 𝐕𝐈𝐏 ⭐
+╰───────────────╯
+╭────────────────╮
+│${prefix}peshub [txt]
+│${prefix}fotopraperfil_femino
+│${prefix}meme
+│${prefix}lomotifs
+│${prefix}pookemon
+│${prefix}game_wallp
+│${prefix}cosplay
+│${prefix}aesthetic
+│${prefix}cosplaysagiri
+│${prefix}nime
+│${prefix}satanic
+│${prefix}glasses
+│${prefix}bdsm
+│${prefix}glasses
+│${prefix}lbc
+│${prefix}hentai4
+│${prefix}hentai3
+│${prefix}ddd
+│${prefix}gerarcpf
+│${prefix}namoro
+│${prefix}igstalk
+│${prefix}vasco
+│${prefix}checarprem
+│${prefix}perfilzap
+│${prefix}editroxo
+│${prefix}editvermelha
+│${prefix}editbranca
+│${prefix}nime
+│${prefix}shotas
+│${prefix}cosplayloli
+│${prefix}pornovid
+│${prefix}pornohub
+│${prefix}hentaistuber
+│${prefix}hentaiword
+│${prefix}assistirhentai
+│${prefix}pornogratis
+│${prefix}xvideos 
+│${prefix}explanadas
+│${prefix}gerargp [amizade]
+│${prefix}pornovid
+│${prefix}vazados
+│${prefix}hentai
+│${prefix}hentai2
+│${prefix}hentai3
+│${prefix}hentai4
+│${prefix}achego
+│${prefix}jahy
+│${prefix}waifu
+│${prefix}glasses
+│${prefix}orgy
+│${prefix}ero
+│${prefix}foto_aleatoria
+│${prefix}fotinhas
+│${prefix}foto18
+│${prefix}video18
+│${prefix}ship 
+│${prefix}affect 
+│${prefix}mms 
+│${prefix}bobross 
+│${prefix}bolsonaro 
+│${prefix}comunismo 
+│${prefix}duelo 
+│${prefix}blur 
+│${prefix}circle 
+│${prefix}del 
+│${prefix}lgbt 
+│${prefix}beautiful 
+│${prefix}invert 
+│${prefix}facepalm 
+│${prefix}jail 
+│${prefix}rip 
+│${prefix}pixelate 
+│${prefix}sepia 
+│${prefix}trash 
+│${prefix}wanted 
+│${prefix}wasted
+│${prefix}wallpaper1
+│${prefix}wallpaper2
+│${prefix}wallpaper3
+│${prefix}wallpaper4
+│${prefix}wallpaper5
+│${prefix}wallpaper6
+│${prefix}wallpaper7
+│${prefix}wallpaper8
+│${prefix}wallpaper9
+│${prefix}addvip
+│${prefix}listavip
+│${prefix}plaq "seu nome"
+│${prefix}plaq2 "seu nome"
+│${prefix}plaq3 "seu nome"
+│${prefix}plaq4 "seu nome"
+│${prefix}plaq5 "seu nome"
+│${prefix}plaq6 "seu nome"
+│${prefix}plaq7 "seu nome"
+│${prefix}plaq8 "seu nome"
+│${prefix}plaq9 "seu nome"
+│${prefix}plaq10 "seu nome"
+│${prefix}plaq11 "seu nome"
+│${prefix}plaq12 "seu nome"
+│${prefix}plaq13 "seu nome"
+│${prefix}plaq14 "seu nome"
+│${prefix}plaq15 "seu nome"
+│${prefix}plaq16 "seu nome"
+│${prefix}loli
+│${prefix}pussy
+│${prefix}neko2
+│${prefix}foot
+│${prefix}gif
+│${prefix}beijo
+│${prefix}lbc
+│${prefix}zettai
+│${prefix}manga
+│${prefix}bdsm
+│${prefix}only1
+│${prefix}only2
+│${prefix}only3
+│${prefix}only4
+│${prefix}only5
+│${prefix}only6
+│${prefix}only7
+│${prefix}only8
+│${prefix}only9
+│${prefix}only10
+│${prefix}only11
+│${prefix}only12
+│${prefix}hentai_paizuri
+│${prefix}hentai_oral
+│${prefix}hentai_ecchi
+│${prefix}hentai_aleatorio
+│${prefix}hentaizinho
+│${prefix}hentai_milf
+│${prefix}hentai_ass
+│${prefix}hentai_neko
+╰─────────────────
+`
+ }
+ 
+ 
